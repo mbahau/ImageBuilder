973da81e5590ba507dc4f862343be07cb7dbbb31
MD BAHAUDDIN <56166977+mbahau@users.noreply.github.com>
2024-10-21 17:44:41 +0000

    update

--- DataGatherFromLinkdin.py
+++ DataGatherFromLinkdin.py
@@ -1,13 +1,22 @@
 # install dependencies
+pip install selenium
 
 # Dependencies
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\md.bahauddin\OneDrive - o9 Solutions\python  projects\ImageBuilder\chromebrowser\chromedriver_win32')
+s
+# Specify the path to chromedriver
+chrome_driver_path = r'C:\Users\md.bahauddin\OneDrive - o9 Solutions\python  projects\ImageBuilder\chromebrowser\chromedriver_win32'
+
+# Create the Service object
+service = Service(executable_path=chrome_driver_path)
+
+driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
 driver.get('https://www.linkedin.com/login')
 
 email_input = driver.find_element(By.NAME, 'session_key')
